997f2e52d1ff0b1159734158b4163c042bd59cf2
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-11-06 11:21:50 +0100

    Test

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Salut le monde !');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
